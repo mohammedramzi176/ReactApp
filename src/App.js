9062564125d94e2f2b095e5dcbbfb9b682de3fbc
ramzi <ramzikvrafikv@gmail.com>
2021-05-01 10:48:42 +0530

    modules added for react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+import functionalComp from "./functionalComp"
+function App() {
+  return (
+    <div>
+      <p>hello</p>
+      <functionalComp></functionalComp>
+    </div>
+    
+  );
+  
+}
+
+export default App;
---
+++ src/functionalComp.js
@@ -0,0 +1,10 @@
+import React from "react"
+function functionalComp(){
+    return
+    (
+        <div>
+            <h1> this is a functional component</h1>
+        </div>
+    )
+}
+export default functionalComp
